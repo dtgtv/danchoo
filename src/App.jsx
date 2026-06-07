ac926f84eb3f413bf821874e5ee0454ca4f920b7
hyperlee <leeharu039@gmail.com>
2026-06-07 04:25:05 -0700

    fix: remove onboard page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './hooks/useAuth'
 import MapPage from './pages/MapPage'
 import PropertyDetailPage from './pages/PropertyDetailPage'
-import { LoginPage, OnboardPage, DashboardPage, PropertyFormPage } from './pages/AgentPages'
+import { LoginPage, DashboardPage, PropertyFormPage } from './pages/AgentPages'
 import './index.css'
 
 function RequireAgent({ children }) {
@@ -24,8 +24,7 @@ function AppRoutes() {
     <Routes>
       <Route path="/" element={<MapPage />} />
       <Route path="/property/:id" element={<PropertyDetailPage />} />
-      <Route path="/login" element={<RequireNoAuth><LoginPage /></RequireNoAuth>} />
-      <Route path="/onboard" element={<OnboardPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/agent" element={<RequireAgent><DashboardPage /></RequireAgent>} />
       <Route path="/agent/property/new" element={<RequireAgent><PropertyFormPage /></RequireAgent>} />
       <Route path="/agent/property/:id/edit" element={<RequireAgent><PropertyFormPage /></RequireAgent>} />
